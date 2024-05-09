7f9d376ae05e241d7e35af656fc6a3a4a4996f7d
travmonkey <travis@batk.me>
2024-05-09 12:25:12 -0500

    Add instructions for gp.nvim

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -2,6 +2,8 @@ return {
 	"robitx/gp.nvim",
 	config = function()
 		require("gp").setup({
+      -- Make sure you change this to your home directory
+      -- Put your openai_api_key in the file and chmod 400 it for security
 			openai_api_key = { "cat", "/home/travis/.open_api_key" },
 		})
 	end,
