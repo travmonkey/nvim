48da5856f188ed546c316ecacdecc4fcf680a586
travmonkey <t.coding@batk.me>
2024-10-07 17:19:09 -0600

    Color LspInfoBorder

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -7,9 +7,11 @@ return {
       contrast = "soft",
       vim.cmd.colorscheme('gruvbox'),
       vim.cmd([[highlight NormalFloat guibg=#282828]]),
+      vim.cmd([[highlight LspInfoBorder guifg=#ebdbb2]]),
       vim.cmd([[highlight NoiceCmdlinePopup guibg=#282828]]),
       vim.cmd([[highlight NoiceCmdlinePopupBorder guibg=#282828]]),
       vim.cmd([[highlight NoiceCmdlinePopupBorder guifg=#83a598]]),
+
     })
   end,
 }
--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -16,6 +16,9 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+      require('lspconfig.ui.windows').default_options = {
+        border = "rounded",
+      }
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
